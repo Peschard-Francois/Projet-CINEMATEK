e668730249f0439090498bb5cceea05c1e880d26
Peschard-Francois <francois.glenard@gmail.com>
2022-09-08 16:09:56 +0200

    Create FunctionIndex for all movies and install Theme Bootswatch

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/movie',name: 'app_movie')]
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(MovieRepository $movieRepository): Response
     {
+        $movies = $movieRepository->findAll();
+
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $movies,
         ]);
     }
 }
