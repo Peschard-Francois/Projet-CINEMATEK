81d59a06679dc3720432355b15bf4a195f64644c
Peschard-Francois <francois.glenard@gmail.com>
2022-09-08 16:41:18 +0200

    Create function for Actor and Create template Twig

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ActorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/actor', name: 'app_actor_')]
 class ActorController extends AbstractController
 {
-    #[Route('/actor', name: 'app_actor')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(ActorRepository $ActorRepository): Response
     {
+        $actors = $ActorRepository->findAll();
+
         return $this->render('actor/index.html.twig', [
-            'controller_name' => 'ActorController',
+            'actors' => $actors,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show')]
+    public function show(int $id ,ActorRepository $ActorRepository): Response
+    {
+        $actorById = $ActorRepository->find($id);
+
+        return $this->render('actor/show.html.twig', [
+            'actorById' => $actorById,
         ]);
     }
 }
